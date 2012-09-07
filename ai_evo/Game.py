bb86e3c67a48274a4f719bb42ecc9de986fd7a27
Akhil Kapoor <akhil.kapoor876@gmail.com>
2012-09-07 08:45:54 -0400

    Added the Graphics

--- ai_evo/Game.py
+++ ai_evo/Game.py
@@ -7,6 +7,11 @@ from copy import deepcopy
 from Player import Player
 from Heuristic import Heuristic
 
+import pygame, sys
+from pygame.locals import *
+
+WHITE   = (255, 255, 255)
+
 class Game(object):
 
     p1 = None
@@ -15,10 +20,13 @@ class Game(object):
     board = []
     prev_board = []
     game = 0
+    D = 0
     
+
     def __init__(self, player1, player2, s):
         self.p1 = player1
         self.p2 = player2
+        self.D = s
 
     def initialize_board(self, n):
         self.size = n
@@ -60,12 +68,36 @@ class Game(object):
         print
         
     def play(self):
-        c = 0
+        
+        pygame.init()
+
+        fpsClock = pygame.time.Clock()
+
+        FPS = 30 # frames per second setting
+        L_GREEN = (  0, 220,   0)
+        N_BLUE  = (  0,   0, 128)
+        PURPLE  = (200,   0, 200)
+        RED     = (255,   0,   0)
+
+
+        DISPLAYSURF = pygame.display.set_mode((self.size*self.D, self.size*self.D))
+        pygame.display.set_caption('Tron!')
+
+        D = self.D
+
+        pygame.draw.line(DISPLAYSURF, WHITE, (0, 0), (0, self.size*D), D)
+        pygame.draw.line(DISPLAYSURF, WHITE, (0, 0), (self.size*D, 0), D)
+        pygame.draw.line(DISPLAYSURF, WHITE, (0, self.size*D), (self.size*D, self.size*D), D)
+        pygame.draw.line(DISPLAYSURF, WHITE, (self.size*D, 0), (self.size*D, self.size*D), D)
+
         while (not self.game):
 
             move1 = self.p1.pick_move(self.board, self.p2.pos)
             move2 = self.p2.pick_move(self.board, self.p1.pos)
             old_board = deepcopy(self.board)
+
+            self.p1.prev_pos = self.p1.pos[:]
+            self.p2.prev_pos = self.p2.pos[:]
 
             temp1 = self.p1.apply_move(move1, old_board)
             temp2 = self.p2.apply_move(move2, old_board)
@@ -73,35 +105,62 @@ class Game(object):
             self.board = self.p1.make_move(move1, self.board)
             self.board = self.p2.make_move(move2, self.board)
 
-            self.describe_board(self.board)
+            pygame.draw.line(DISPLAYSURF, L_GREEN, (self.p1.prev_pos[1]*D, self.p1.prev_pos[0]*D), (self.p1.pos[1]*D, self.p1.pos[0]*D), D)
+            pygame.draw.line(DISPLAYSURF, PURPLE, (self.p2.prev_pos[1]*D, self.p2.prev_pos[0]*D), (self.p2.pos[1]*D, self.p2.pos[0]*D), D)
+            pygame.display.update()
+            fpsClock.tick(FPS)
+        
+            #self.describe_board(self.board)
 
             if self.is_equal(old_board, self.board):
                 # trying to step on each other's head
 #                self.game = 3
                 print 'draw1'
+                do_something()
                 return None
             elif self.is_equal(temp1, temp2):
                 # trying to get to the same spot
 #                self.game = 3
                 print 'draw2'
+                do_something()
                 return None
+            
             elif self.is_equal(temp1, old_board):
                 # only p1 crashed
 #                self.game = 2
                 print 'p1 crashed'
+                do_something()
                 return self.p2
             elif self.is_equal(temp2, old_board):
                 # only p2 crashed
 #                self.game = 1
                 print 'p2 crashed'
+                do_something()
                 return self.p1
+            
 
 
             #self.describe_board(self.board)
-#            self.board = final
-            c += 1
-#            if c % 2 == 0:
+            #self.board = final
+            #c += 1
+            #if c % 2 == 0:
             
         # just staying here but never executes.
         return random.choice([self.player1, self.player2, None])
     
+
+def do_something():
+
+    fpsClock = pygame.time.Clock()
+    FPS = 30 # frames per second setting
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+            fpsClock.tick(FPS)
+        pygame.display.update()
+        fpsClock.tick(FPS)
