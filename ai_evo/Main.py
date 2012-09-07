24d7dc11842e84f965f81f7b44da32bd6f52c57a
Akhil Kapoor <akhil.kapoor876@gmail.com>
2012-09-07 04:00:45 -0400

    fixed the imports

--- ai_evo/Main.py
+++ ai_evo/Main.py
@@ -1,7 +1,10 @@
 '''
 @author: Michael Smith, Akhil Kapoor
 '''
-from ai_evo.Evolution import Evolution
+from Evolution import Evolution
+from Game import Game
+from Player import Player
+from Heuristic import Heuristic
 
 class TronEvo(object):
     '''
@@ -19,3 +22,17 @@ class TronEvo(object):
     def runEvolution(self):
         tronEvo = Evolution
         tronEvo.run()
+		
+    def test_game(self):
+        p1 = Player()
+        p1.name = 'p1'
+        p1.pos = [5,2]
+        p1.heuristic = Heuristic()
+        p2 = Player()
+        p2.name = 'p2'
+        p2.pos = [5,7]
+        p2.heuristic = Heuristic()
+        g = Game(p1, p2, 20)
+        g.initialize_board(10)
+        g.describe_board(g.board)
+        g.play()
